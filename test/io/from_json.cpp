17c7ee11ccc65958a5ece2f057578f09bc5ce7b5
aguang <aguang.xyz@gmail.com>
2020-04-16 16:24:48 +1200

    Add test case for fdc::from_json

--- test/io/from_json.cpp
+++ test/io/from_json.cpp
@@ -1,7 +1,22 @@
 #include "gtest/gtest.h"
 #include "fdc/fdc.h"
 
+#include <fstream>
+
+using namespace std;
+using namespace fdc;
+
 TEST(io, from_json) {
 
-  EXPECT_EQ(10, 10);
+  ifstream abalone;
+
+  abalone.open("../../dataset/A/Complete Data/abalone.json");
+
+  attrs U;
+  fds F;
+
+  fdc::from_json(abalone, U, F);
+  
+  cout<<"U = "<<to_str(U)<<endl;
+  cout<<"F = "<<to_str(F)<<endl;
 }
