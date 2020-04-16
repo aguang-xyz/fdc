d2a1a534e164fdff28f24cc522b72f252cf7fed8
aguang <aguang.xyz@gmail.com>
2020-04-16 16:33:48 +1200

    Add assertations in test case io.from_json

--- test/io/from_json.cpp
+++ test/io/from_json.cpp
@@ -8,15 +8,28 @@ using namespace fdc;
 
 TEST(io, from_json) {
 
-  ifstream abalone;
+  ifstream iris;
 
-  abalone.open("../../dataset/A/Complete Data/abalone.json");
+  iris.open("../../dataset/A/Complete Data/iris.json");
 
-  attrs U;
-  fds F;
+  auto U1 = attrs();
+  auto F1 = fds();
 
-  fdc::from_json(abalone, U, F);
+  fdc::from_json(iris, U1, F1);
+
+  auto U2 = attrs({ "0","1","2","3","4" });
+
+  auto F2 = fds({
+ 
+    fd(attrs({ "0", "1", "2" }), attrs({ "4" })),
+    fd(attrs({ "0", "1", "3" }), attrs({ "4" })),
+    fd(attrs({ "0", "2", "3" }), attrs({ "4" })),
+    fd(attrs({ "1", "2", "3" }), attrs({ "4" })),
+  });
   
-  cout<<"U = "<<to_str(U)<<endl;
-  cout<<"F = "<<to_str(F)<<endl;
+  cout<<"U = "<<to_str(U1)<<endl;
+  cout<<"F = "<<to_str(F1)<<endl;
+
+  ASSERT_EQ(U1, U2);
+  ASSERT_EQ(F1, F2);
 }
