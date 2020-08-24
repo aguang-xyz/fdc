8f7a44c2139c648218297aba5c098e9ae250836d
Guangrui Wang <aguang.xyz@gmail.com>
2020-08-24 16:46:26 +1200

    Add test case for Quine-McCluskey method.

--- test/algorithm/qmc.cpp
+++ test/algorithm/qmc.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 using namespace fdc;
 
-TEST(algorithm, qmc) {
+TEST(algorithm, qmc_1) {
 
   bool_exprs exprs = bool_exprs({
 
@@ -24,5 +24,41 @@ TEST(algorithm, qmc) {
                            bool_expr({'-', '-', '0', '0'}),
                            bool_expr({'0', '1', '-', '1'}),
                            bool_expr({'1', '-', '1', '1'}),
+                       }));
+}
+
+TEST(algorithm, qmc_2) {
+
+  // This case:
+  //
+  //   AB -> C             AB -> C
+  //   AB -> D     -->      C -> D
+  //    C -> D
+
+  bool_exprs exprs = bool_exprs({
+
+      // AB -> C
+      bool_expr({'1', '1', '0', '0'}),
+      bool_expr({'1', '1', '0', '1'}),
+
+      // AB -> D
+      bool_expr({'1', '1', '0', '0'}),
+      bool_expr({'1', '1', '1', '0'}),
+
+      // C -> D
+      bool_expr({'0', '0', '1', '0'}),
+      bool_expr({'0', '1', '1', '0'}),
+      bool_expr({'1', '0', '1', '0'}),
+      bool_expr({'1', '1', '1', '0'}),
+  });
+
+  bool_exprs min_exprs = qmc(exprs);
+
+  ASSERT_EQ(min_exprs, bool_exprs({
+                           // C -> D
+                           bool_expr({'-', '-', '1', '0'}),
+
+                           // AB -> C
+                           bool_expr({'1', '1', '0', '-'}),
                        }));
 }
