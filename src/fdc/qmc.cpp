2cf327f00ee90f7eecdb1b719e5cd7dd0a24f3b0
Guangrui Wang <aguang.xyz@gmail.com>
2020-08-25 20:06:03 +1200

    Integrate with GLPK.

--- src/fdc/qmc.cpp
+++ src/fdc/qmc.cpp
@@ -2,89 +2,49 @@
 #include <set>
 
 #include "fdc.h"
+#include "glpk/glpk.h"
 
 namespace fdc {
 
 using namespace std;
 
-struct qmc_reduce_state {
+typedef pair<set<int>, bool_expr> qmc_combined_expr;
 
-  // Related input expression ids.
-  set<int> ids;
+// Remove redundant expressions.
+bool_exprs qmc_redundant_filter(bool_exprs exprs) {
 
-  // Reduced expression.
-  bool_expr expr;
+  set<bool_expr> expr_set;
 
-  friend bool operator<(const qmc_reduce_state &x, const qmc_reduce_state &y) {
-    return x.expr < y.expr;
-  }
-};
-
-struct qmc_search_state {
-
-  // Covered input expression ids.
-  set<int> ids;
-
-  // Combined states.
-  set<qmc_reduce_state> states;
-
-  // Combined state ids.
-  set<int> state_ids;
-
-  static int count_attrs(const qmc_search_state &x) {
-
-    int count = 0;
-
-    for (auto state : x.states) {
-      for (auto c : state.expr) {
-        if (c == '0' || c == '1') {
-          count++;
-        }
-      }
-    }
-
-    return count;
+  for (auto expr : exprs) {
+    expr_set.insert(expr);
   }
 
-  friend bool operator<(const qmc_search_state &x, const qmc_search_state &y) {
-
-    if (x.states.size() < y.states.size()) {
-      return true;
-    }
-
-    if (x.states.size() > y.states.size()) {
-      return false;
-    }
-
-    return count_attrs(x) < count_attrs(y);
-  }
-
-  friend bool operator>(const qmc_search_state &x, const qmc_search_state &y) {
-    return y < x;
-  }
-};
-
-bool_exprs qmc_non_redundant(bool_exprs input_exprs) {
-
-  set<bool_expr> s;
-
-  bool_exprs exprs;
-
-  for (auto expr : input_exprs) {
-    if (s.find(expr) == s.end()) {
-      s.insert(expr);
-      exprs.push_back(expr);
-    }
-  }
-
-  return exprs;
+  return bool_exprs(expr_set.begin(), expr_set.end());
 }
 
-int qmc_count_zero(bool_expr expr) {
+// Check if a and b has only one different element which are '0' and '1'.
+bool qmc_match_expr(bool_expr a, bool_expr b, int &pos) {
+
+  pos = -1;
+
+  for (int i = 0; i < a.size(); i++) {
+    if (a[i] != b[i]) {
+      if (a[i] == '0' && b[i] == '1' && pos == -1) {
+        pos = i;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return pos != -1;
+}
+
+int qmc_count_zero(bool_expr x) {
 
   int count = 0;
 
-  for (char &c : expr) {
+  for (auto c : x) {
     if (c == '0') {
       count++;
     }
@@ -93,243 +53,128 @@ int qmc_count_zero(bool_expr expr) {
   return count;
 }
 
-bool qmc_match(bool_expr expr1, bool_expr expr2, int &pos) {
+int qmc_count_attributes(bool_expr x) {
 
-  pos = -1;
+  int count = 0;
 
-  for (int i = 0; i < expr1.size(); i++) {
-    if (expr1[i] != expr2[i]) {
+  for (auto c : x) {
+    if (c == '0' || c == '1') {
+      count++;
+    }
+  }
 
-      if (pos == -1 && expr1[i] == '1' && expr2[i] == '0') {
-        pos = i;
-      } else {
-        return false;
+  return count;
+}
+
+// Try to combine expressions.
+vector<qmc_combined_expr> qmc_combine(int N, bool_exprs exprs) {
+
+  set<qmc_combined_expr> reduced_set;
+  set<qmc_combined_expr> buckets[N + 1][N + 1];
+
+  for (int id = 0; id < exprs.size(); id++) {
+    buckets[0][qmc_count_zero(exprs[id])].insert(
+        qmc_combined_expr(set<int>({id}), exprs[id]));
+  }
+
+  for (int num_dash = 0; num_dash < N; num_dash++) {
+    for (int num_zero = 0; num_zero < N; num_zero++) {
+      for (auto a : buckets[num_dash][num_zero + 1]) {
+        for (auto b : buckets[num_dash][num_zero]) {
+
+          int pos = 0;
+
+          if (qmc_match_expr(a.second, b.second, pos)) {
+
+            set<int> new_ids = set<int>(a.first.begin(), a.first.end());
+
+            for (auto id : b.first) {
+              new_ids.insert(id);
+            }
+
+            bool_expr new_expr = bool_expr(a.second);
+            new_expr[pos] = '-';
+
+            buckets[num_dash + 1][num_zero].insert(qmc_combined_expr({
+                .ids = new_ids,
+                .expr = new_expr,
+            }));
+
+            reduced_set.insert(a);
+            reduced_set.insert(b);
+          }
+        }
       }
     }
   }
 
-  if (pos == -1) {
-    return false;
+  vector<qmc_combined_expr> result;
+
+  for (int num_dash = 0; num_dash < N; num_dash++) {
+    for (int num_zero = 0; num_zero < N; num_zero++) {
+      for (auto a : buckets[num_dash][num_zero]) {
+        if (reduced_set.find(a) == reduced_set.end()) {
+          result.push_back(a);
+        }
+      }
+    }
   }
 
-  return true;
+  return result;
 }
 
-bool_exprs qmc(bool_exprs input_exprs) {
+bool_exprs qmc_search(bool_exprs exprs,
+                      vector<qmc_combined_expr> combined_exprs) {
 
-  bool_exprs exprs = qmc_non_redundant(input_exprs);
+  set<int> refs[exprs.size()];
+
+  glp_prob *lp = glp_create_prob();
+
+  // TODO
+
+  return exprs;
+}
+
+bool_exprs qmc(bool_exprs exprs) {
 
   if (exprs.size() == 0) {
     return exprs;
   }
 
-  fprintf(stderr, "input size = %lu\n", input_exprs.size());
+  int N = exprs[0].size();
 
-  // The number of columns.
-  const int N = exprs[0].size();
+  // 1. Remove redundant expressions.
+  exprs = qmc_redundant_filter(exprs);
 
-  // Buckets, buckets[count_zero(expr)] <- expr.
-  set<qmc_reduce_state> buckets[N + 1][N + 1];
+  // 2. Try to combine and reduce expressions.
+  auto combined_exprs = qmc_combine(N, exprs);
 
-  // Initialize buckets.
-  for (int i = 0; i < exprs.size(); i++) {
-    buckets[0][qmc_count_zero(exprs[i])].insert(qmc_reduce_state({
-        .ids = set<int>({i}),
-        .expr = bool_expr(exprs[i]),
-    }));
+  // For debug.
+  for (auto x : combined_exprs) {
+
+    for (auto c : x.second) {
+      printf("%c", c);
+    }
+
+    printf(" : ");
+
+    for (int id = 0; id < exprs.size(); id++) {
+      if (x.first.find(id) != x.first.end()) {
+        printf("x");
+      } else {
+        printf(" ");
+      }
+    }
+
+    for (auto id : x.first) {
+      printf(" %d", id);
+    }
+
+    printf("\n");
   }
 
-  set<qmc_reduce_state> reduced_states;
-
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
-      for (auto x : buckets[i][j]) {
-        for (auto y : buckets[i][j + 1]) {
-
-          int pos;
-
-          if (qmc_match(x.expr, y.expr, pos)) {
-
-            reduced_states.insert(x);
-            reduced_states.insert(y);
-
-            set<int> new_ids = set<int>();
-
-            for (auto id : x.ids) {
-              new_ids.insert(id);
-            }
-
-            for (auto id : y.ids) {
-              new_ids.insert(id);
-            }
-
-            bool_expr new_expr = bool_expr(x.expr);
-
-            new_expr[pos] = '-';
-
-            buckets[i + 1][j].insert(qmc_reduce_state({
-                .ids = new_ids,
-                .expr = new_expr,
-            }));
-          }
-        }
-      }
-    }
-  }
-
-  // Extract reduced states.
-  vector<qmc_reduce_state> rest_states;
-
-  for (int i = 0; i <= N; i++) {
-    for (int j = 0; j <= N; j++) {
-      for (auto state : buckets[i][j]) {
-        if (reduced_states.find(state) == reduced_states.end()) {
-          rest_states.push_back(state);
-        }
-      }
-    }
-  }
-
-  fprintf(stderr, "Start searching, |rest_states| = %lu.\n",
-          rest_states.size());
-
-  for (auto state : rest_states) {
-    for (auto c : state.expr) {
-      fprintf(stderr, "%c", c);
-    }
-
-    fprintf(stderr, " :");
-
-    for (auto id : state.ids) {
-      fprintf(stderr, " %d", id);
-    }
-
-    fprintf(stderr, "\n");
-  }
-
-  // Search for the optimal combination.
-  priority_queue<qmc_search_state, vector<qmc_search_state>,
-                 greater<qmc_search_state>>
-      queue;
-
-  set<set<int>> visit;
-
-  auto init_state = qmc_search_state({
-      .ids = set<int>(),
-      .states = set<qmc_reduce_state>(),
-      .state_ids = set<int>(),
-  });
-
-  queue.push(init_state);
-
-  while (queue.size() > 0) {
-    fprintf(stderr, "Rest: %lu\n", queue.size());
-
-    auto top = queue.top();
-    queue.pop();
-
-    if (top.ids.size() == exprs.size()) {
-      // Since it is an A* like searching process, the first valid answer is
-      // the optimum answer.
-
-      bool_exprs result;
-
-      for (auto state : top.states) {
-        result.push_back(state.expr);
-      }
-
-      return result;
-    }
-
-    // Count how many un-searched expressions can cover each target input
-    // expression.
-    int ref_cnt[exprs.size()] = {0};
-
-    for (int state_id = 0; state_id < rest_states.size(); state_id++) {
-      if (top.state_ids.find(state_id) == top.state_ids.end()) {
-        for (auto id : rest_states[state_id].ids) {
-          ref_cnt[id]++;
-        }
-      }
-    }
-
-    // Compute if there is a must included search expression.
-    int must_select_state_id = -1;
-
-    for (int state_id = 0; state_id < rest_states.size(); state_id++) {
-      if (top.state_ids.find(state_id) == top.state_ids.end()) {
-        int must_select = false;
-
-        for (auto id : rest_states[state_id].ids) {
-          if (ref_cnt[id] == 1 && top.ids.find(id) == top.ids.end()) {
-            must_select = true;
-            break;
-          }
-        }
-
-        if (must_select) {
-          must_select_state_id = state_id;
-          break;
-        }
-      }
-    }
-
-    // Iteration for adding a new expression.
-    for (int state_id = 0; state_id < rest_states.size(); state_id++) {
-
-      // If there one must be included, skip others.
-      if (must_select_state_id != -1 && must_select_state_id != state_id) {
-        continue;
-      }
-
-      auto state = rest_states[state_id];
-
-      if (top.state_ids.find(state_id) == top.state_ids.end()) {
-
-        auto new_ids = set<int>();
-
-        for (auto id : top.ids) {
-          new_ids.insert(id);
-        }
-
-        bool expand_ids = false;
-
-        for (auto id : state.ids) {
-          if (new_ids.find(id) == new_ids.end()) {
-            new_ids.insert(id);
-            expand_ids = true;
-          }
-        }
-
-        // Prune strategy 1. Adding new expressions must lead to more input
-        // expressions to be covered.
-        if (!expand_ids) {
-          continue;
-        }
-
-        set<qmc_reduce_state> new_states(top.states.begin(), top.states.end());
-        new_states.insert(state);
-
-        set<int> new_state_ids(top.state_ids.begin(), top.state_ids.end());
-        new_state_ids.insert(state_id);
-
-        auto new_search_state = qmc_search_state({
-            .ids = new_ids,
-            .states = new_states,
-            .state_ids = new_state_ids,
-        });
-
-        // Prune strategy 2. Do not search for exact same combinations for
-        // multiple times.
-        if (visit.find(new_state_ids) == visit.end()) {
-          queue.push(new_search_state);
-          visit.insert(new_state_ids);
-        }
-      }
-    }
-  }
-
-  // Failed.
-  return exprs;
+  // 3. Search for the best selection of combined expressions.
+  return qmc_search(exprs, combined_exprs);
 }
+
 } // namespace fdc
